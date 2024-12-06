1063da542219953e7b5fd44b3cca6c38b8ea6144
Besthope <874256269@qq.com>
2024-12-06 15:48:15 +0800

    feat: impl lab1

--- game/server.py
+++ game/server.py
@@ -3,36 +3,54 @@ from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 
+@app.route('/')
 def index():
-    pass
+    return redirect(url_for('game'))
 
 
+@app.route('/game')
 def game():
-    pass
+    return render_template('2048.html')
 
 
-def no_url_found():
-    pass
+@app.errorhandler(404)
+def no_url_found(error):
+    return 'Page not found', 404
 
 
-def add_in_url():
-    pass
+@app.route('/add/<int:num1>/<int:num2>')
+def add_in_url(num1, num2):
+    return str(num1 + num2)
 
 
-def take_url_args():
-    pass
+@app.route('/check/<any(green, blue, red):color>')
+def take_url_args(color):
+    return color
 
 
+@app.route('/query', methods=['GET'])
 def take_query_str():
-    pass
+    name = request.args.get('name')
+    return f"Hello {name if name else 'Anonymous'}"
 
 
+@app.route('/data', methods=['POST'])
 def take_form_data():
-    pass
+    data = request.form
+    name = data.get('name')
+    return f"Hello {name if name else 'Anonymous'}"
 
 
+@app.route('/upload', methods=['POST'])
 def file_upload():
-    pass
+    file = request.files['file']
+    if file:
+        # check file size
+        if len(file.read()) > 1024 * 1024 * 2:
+            return 'File size exceeds limit of 2MB'
+        return 'File uploaded successfully'
+    else:
+        return 'No file uploaded'
 
 
 if __name__ == '__main__':
